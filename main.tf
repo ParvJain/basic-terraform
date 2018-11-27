a301e7a9de52d6d627920a7b508d5796fda62400
Parv Jain <parv.wutizup@gmail.com>
2018-11-27 12:02:33 +0530

    Modified main.tf, intialized aws secret variable

--- main.tf
+++ main.tf
@@ -1,7 +1,10 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
 provider "aws" {
   region = "eu-west-1"
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
 }
 
 resource "aws_vpc" "default" {
