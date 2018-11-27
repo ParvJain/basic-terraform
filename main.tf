100a240af9cb22e751f960bcc757051cb7d5c565
Parv Jain <parv.wutizup@gmail.com>
2018-11-27 11:52:18 +0530

    Modified main.tf, cleaned up

--- main.tf
+++ main.tf
@@ -70,7 +70,6 @@ resource "aws_route_table" "eu-west-1a-private" {
     }
 }
 
-
 resource "aws_route_table_association" "eu-west-1a-private" {
     subnet_id = "${aws_subnet.eu-west-1a-private.id}"
     route_table_id = "${aws_route_table.eu-west-1a-private.id}"
